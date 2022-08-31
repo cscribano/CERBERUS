5493715e81f0524bea11347555ed53fb3c8f832b
cscribano <43755732+cscribano@users.noreply.github.com>
2022-08-31 14:19:01 +0200

    Update deps.sh

--- docs/deps.sh
+++ docs/deps.sh
@@ -1,26 +1,4 @@
-#!/bin/bash
-set -e 
-
-sudo ln -sf /usr/bin/python3 /usr/bin/python
-
-# pip
-sudo apt-get update
-sudo apt-get install -y python3-pip
-
-# Jtop
-sudo -H pip install -U jetson-stats
-
-# Set up CUDA environment
-if [ ! -x "$(command -v nvcc)" ]; then
-    echo "export PATH=/usr/local/cuda/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
-    echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ~/.bashrc
-    # Trick
-    export PATH=/usr/local/cuda/bin\${PATH:+:\${PATH}}
-    export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}
-fi
-
-# NumPy
-sudo apt-get install -y python3-pip libhdf5-serial-dev hdf5-tools libcanberra-gtk-module
+sudo apt-get install -y libhdf5-serial-dev hdf5-tools libcanberra-gtk-module
 sudo -H pip3 install Cython
 sudo pip3 -H install numpy==1.19
 
@@ -41,4 +19,3 @@ sudo -H CUPY_NVCC_GENERATE_CODE="current" CUPY_NUM_BUILD_JOBS=$(nproc) pip3 inst
 echo " ===================================="
 echo " |       REBOOT IS REQUIRED         |"
 echo " ===================================="
-
